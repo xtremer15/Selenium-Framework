d22ea778f9a3afffd35b9e4c570e5a3956e7cb7c
Mihai <falen.angel12@yahoo.com>
2022-12-10 18:37:14 +0200

    Started working to run tests in parallel

--- src/main/java/runner/ui/UiRunner.java
+++ src/main/java/runner/ui/UiRunner.java
@@ -4,6 +4,7 @@ import io.cucumber.junit.Cucumber;
 //import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.DataProvider;
 //import org.junit.runner.RunWith;
 
 
@@ -11,9 +12,14 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
         features = {"src/test/resources/features/ui"},
         glue = {"ContextConfig/UI", "Steps"},
-        publish = true,
+        publish = false,
         plugin = {"pretty", "junit:target/junit-reports/", "json:target/cucumber/cucumber-reports.json"}
 )
 public class UiRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
 
