4428c3e3da51028d9f6c2258d3a756ee437cdf69
Mihai <falen.angel12@yahoo.com>
2022-12-10 05:34:20 +0200

    Added Testng Runner

--- src/main/java/runner/ui/UiRunner.java
+++ src/main/java/runner/ui/UiRunner.java
@@ -5,13 +5,15 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
+import org.testng.junit.JUnit4TestRunner;
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/java/features/ui"},
         glue = {"ContextConfig/UI", "Steps"},
-        publish = false,
-        plugin = {"pretty", "json:target/cucumber/cucumber-reports"}
+        publish = true,
+        plugin = {"pretty", "junit:target/junit-reports/", "json:target/cucumber/cucumber-reports.json"}
 )
 public class UiRunner extends AbstractTestNGCucumberTests {
 }
+
