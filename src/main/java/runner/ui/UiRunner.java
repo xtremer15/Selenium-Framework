82a9b212171a203e2023288076d74827f9c2e2d6
Mihai <falen.angel12@yahoo.com>
2022-12-10 05:42:07 +0200

    Refactor

--- src/main/java/runner/ui/UiRunner.java
+++ src/main/java/runner/ui/UiRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
-import org.testng.junit.JUnit4TestRunner;
+
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(
